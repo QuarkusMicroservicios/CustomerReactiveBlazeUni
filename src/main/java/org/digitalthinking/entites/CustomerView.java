e73497cc14caf4065d27fb0d7ce6041b2fba76d7
joffre <joffre.hermosilla@gmail.com>
2024-04-27 23:03:43 -0500

    chore[0] configuracion jpa blaze reactivo

---
+++ src/main/java/org/digitalthinking/entites/CustomerView.java
@@ -0,0 +1,16 @@
+package org.digitalthinking.entites;
+
+import com.blazebit.persistence.view.EntityView;
+import com.blazebit.persistence.view.IdMapping;
+
+@EntityView(Customer.class)
+public interface CustomerView {
+
+    @IdMapping
+    Long getId();
+
+    String getCode();
+
+    String getAccountNumber();
+
+}
---
+++ src/main/java/org/digitalthinking/repositories/CustomerRepository.java
@@ -0,0 +1,53 @@
+package org.digitalthinking.repositories;
+
+
+import com.blazebit.persistence.CriteriaBuilder;
+import com.blazebit.persistence.CriteriaBuilderFactory;
+import com.blazebit.persistence.view.EntityViewManager;
+import com.blazebit.persistence.view.EntityViewSetting;
+import org.digitalthinking.entites.Customer;
+import org.digitalthinking.entites.CustomerView;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+import java.util.List;
+
+@ApplicationScoped
+public class CustomerRepository {
+    @Inject
+    EntityManager em;
+    @Inject
+    CriteriaBuilderFactory cbf;
+    @Inject
+    EntityViewManager evm;
+
+
+
+    @Transactional
+    public void createdCustomer(Customer p){
+        em.persist(p);
+    }
+
+    @Transactional
+    public void deleteCustomer(Customer p){
+        em.remove(em.contains(p) ? p : em.merge(p));
+    }
+
+    @Transactional
+    public List<CustomerView> listCustomer(){
+       //List<Customer> customers = em.createQuery("select p from Customer p").getResultList();
+        CriteriaBuilder<Customer> cb = cbf.create(em,Customer.class);
+        List<CustomerView> customers = evm.applySetting(EntityViewSetting.create(CustomerView.class), cb).getResultList();
+        return customers;
+    }
+    @Transactional
+    public Customer findCustomer(Long Id){
+        return em.find(Customer.class, Id);
+    }
+    @Transactional
+    public void updateCustomer(Customer p){
+        em.merge(p);
+    }
+}
